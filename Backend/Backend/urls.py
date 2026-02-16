edbeddb99855d58115379035eb62e46d1135705d
Swapnil Reddy Merugu <swapnilreddymerugu@Mac.alphionsee.in>
2026-02-16 15:15:48 +0530

    Fixed home route order

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Expense Tracker Backend is Running 🚀")
+
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/',include('expense.urls')),
 ]
+
+
